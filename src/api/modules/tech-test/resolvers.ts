6f39f7e86d31325201021c21b4366a90df023d8c
Eduardo Turiño <eturino@eturino.com>
2021-04-16 18:07:44 +0100

    small hint

--- src/api/modules/tech-test/resolvers.ts
+++ src/api/modules/tech-test/resolvers.ts
@@ -1,9 +1,11 @@
 import { QueryResolvers } from "@generated-types/schema-types";
 
 const Query: Required<Pick<QueryResolvers, "messageList">> = {
-  messageList: (_root, _args, _context) => {
+  messageList: (_root, args, context) => {
     // TODO: COMPLETE WARM-UP EXERCISE
     // hint: have a look at the `makeFakeSentence` util available in the context;
+    console.log("args", args);
+    console.log("context", context);
     throw "not implemented yet";
   },
 };
